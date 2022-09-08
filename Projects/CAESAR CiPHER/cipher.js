b6acd0d49c1393231209ca74559299fdaccb4440
Sawwe <sawwe.rr19@gmail.com>
2022-09-08 21:27:50 +0200

    Caesar Cypher finished

--- Projects/CAESAR CiPHER/cipher.js
+++ Projects/CAESAR CiPHER/cipher.js
@@ -1,9 +1,19 @@
 let decode_mode=false;
 
+function updateValue(){
+    let rot = document.getElementById("num").value;
+    console.log("funciona ")
+    if(decode_mode){
+        document.getElementById("mode").innerHTML = "DECODE ROT" + rot;
+    }else{
+        document.getElementById("mode").innerHTML = "ENCODE ROT" +  rot;
+    }
+}
+
 function rot(str, num) {
+    num = parseInt(num);
     let result="";
     let arr = str.toUpperCase().split("");
-    console.log(arr);
     let alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
     if(decode_mode){
         console.log("decode")
@@ -14,15 +24,17 @@ function rot(str, num) {
             return newLetter;
           }
           result = arr.map(letter=>( /[A-Z]/.test(letter)? decodeLetter(letter): letter));
+          console.log(result.join(""));
     }else{
+        console.log("encode")
         function encodeLetter(letter){
             let index = alph.indexOf(letter);
-            let newIndex = (index <= 26- num)? index+num: num+index-26;
+            let newIndex = (index + num >= 26)? num + index - 26: num + index;
             let newLetter = alph[newIndex];
             return newLetter;
           }
           result = arr.map(letter=>(/[A-Z]/.test(letter)? encodeLetter(letter): letter));
-    }
+        }
     console.log(result.join(""));
     return result.join("");
   }
@@ -30,15 +42,18 @@ function rot(str, num) {
 function changeMode(){
     if(decode_mode){
         decode_mode = false;
-        document.getElementById("mode").innerHTML = "ENCODE";
+        updateValue();
+        document.getElementById("cipherButton").innerHTML = "ENCODE";
     }else{
         decode_mode = true;
-        document.getElementById("mode").innerHTML = "DECODE";
+        updateValue();
+        document.getElementById("cipherButton").innerHTML = "DECODE";
     }
 }
 
 function cipher(){
     let input = document.getElementById("input").value;
+    console.log(input);
     let num  = document.getElementById("num").value;
     document.getElementById("output").value = rot(input, num);
 }
