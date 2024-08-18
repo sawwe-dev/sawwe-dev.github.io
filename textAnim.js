f434dffc9a8a77dd0e2f953ecb0fcb7daedf86d1
sawwe-dev <sawwe.dev@gmail.com>
2024-08-18 11:57:56 +0200

    Fixed and migrated

--- textAnim.js
+++ textAnim.js
@@ -1,16 +1,15 @@
-let str1 = "high school student.";
+let str1 = "college student.";
 var str2 = "self thaught programmer.";
 var str3 = "web dev.";
-var str4 = "Freelancer.";
-var str5 = "videogame player.";
+var str4 = "freelancer.";
 var startText = "Im a "
-var arrOfStr = [str1, str2, str3, str4, str5];
+var arrOfStr = [str1, str2, str3, str4];
 var arrOfArrs = arrOfStr.map( str => str.split(""));
 var actualArr = 0;
 var i = 1;
 const text = document.getElementById("myDesc");
 var addTiming = 100;
-var timingToRead = 1000;
+var timingToRead = 1500;
 var subTiming = 50;
 
 
@@ -19,9 +18,6 @@ var subTiming = 50;
 function addLetter(){
     setTimeout(function() {   
         text.innerHTML = startText + arrOfArrs[actualArr].slice(0, i).join("");
-        
-
-
         i++;                  
         if (i < arrOfArrs[actualArr].length + 1) {           
           addLetter();             
@@ -35,9 +31,6 @@ function addLetter(){
 function subLetter(){
   setTimeout(function() {   
     text.innerHTML = startText + arrOfArrs[actualArr].slice(0, i).join("");
-    
-
-
     i--;                  
     if (i > 0) {           
       subLetter();             
@@ -48,13 +41,10 @@ function subLetter(){
       }else{
         actualArr = 0;
       }
-      console.log(actualArr)
       
       setTimeout(addLetter, addTiming);
     }                      
   }, subTiming)
 }
-
-
 
 setTimeout(addLetter, 3500);
